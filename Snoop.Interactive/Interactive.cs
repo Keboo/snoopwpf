c0596811dd60d6aafb04156d8b0ed2de6fbdb562
Kevin Bost <kitokeboo@gmail.com>
2020-08-13 20:23:14 -0700

    Work on Nuget to launch interactive

---
+++ Snoop.Interactive/Interactive.cs
@@ -0,0 +1,19 @@
+﻿namespace Snoop.Interactive
+{
+    public class Interactive
+    {
+        private static MethodInfo CallbackMethod { get; }
+            = typeof(Interactive).GetMethod(nameof(OnStart));
+
+        public static void Start(int pid)
+        {
+            InjectorLauncherManager.Launch(pid, CallbackMethod);
+        }
+
+        public static int StartSnoop(string settingsFile)
+        {
+
+            return -1;
+        }
+    }
+}
